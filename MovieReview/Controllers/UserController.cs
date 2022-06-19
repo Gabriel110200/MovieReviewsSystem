9d26f304bea32a074064c669913a7089d7715a57
Gabriel <gb_souza@id.uff.br>
2022-06-19 10:23:16 -0300

    registration done

---
+++ MovieReview/Controllers/UserController.cs
@@ -0,0 +1,75 @@
+﻿
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using MovieReview.Models;
+using System.Threading.Tasks;
+
+namespace MovieReview.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly UserManager<IdentityUser> userManager;
+        private readonly SignInManager<IdentityUser> signInManager;
+
+        private User Model;
+
+        public UserController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+
+
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new IdentityUser()
+                {
+                    UserName = Model.Email,
+                    Email = Model.Email
+                };
+
+                var result = await userManager.CreateAsync(user, Model.Password);
+
+                if (result.Succeeded)
+                {
+                    await signInManager.SignInAsync(user, false);
+                    return RedirectToPage("Index");
+
+                }
+
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+
+
+            }
+
+            return RedirectToPage("Index");
+
+
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        /*
+        public async Task<ActionResult> Register(ApplicationUser u)
+        {
+            var user = new ApplicationUser { UserName = u.UserName, Email = u.Email };
+
+       
+
+           // var result = await UserManager<ApplicationUser>.CreateAsync(user, u.Password);
+
+
+        }
+        */
+
+    }
+}
---
+++ MovieReview/Models/User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MovieReview.Models
+{
+    public class User
+    {
+        public Guid id { get; set; }
+
+        public string UserName { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare(nameof(Password), ErrorMessage = "Password and confirmation password did not match")]
+        public string ConfirmPassword { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+
+    }
+}
