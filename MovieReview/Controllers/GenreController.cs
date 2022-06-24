0b3255d3ad10809e8d11ab27ba0cca01f9da3b62
Gabriel <gb_souza@id.uff.br>
2022-06-23 22:02:39 -0300

    add genre controller and some func

---
+++ MovieReview/Controllers/GenreController.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MovieReview.Models;
+
+namespace MovieReview.Controllers
+{
+    public class GenreController : Controller
+    {
+
+        private readonly AuthDbContext context;
+
+        public GenreController(AuthDbContext context)
+        {
+
+            this.context = context;
+
+        }
+
+        // GET: GenreController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: GenreController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: GenreController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: GenreController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Genre genre)
+        {
+            try
+            {
+                context.genre.Add(genre);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: GenreController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var genre = context.genre.FindAsync(id);
+            return View(genre);
+        }
+
+        // POST: GenreController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Genre genre)
+        {
+            try
+            {
+                context.genre.Update(genre);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: GenreController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: GenreController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
