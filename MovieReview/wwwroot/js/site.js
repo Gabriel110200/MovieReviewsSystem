8ba4205d499914c0e292d955786c7aff3003ece5
Gabriel <gb_souza@id.uff.br>
2022-07-23 23:55:53 -0300

    fix minor js bugs

--- MovieReview/wwwroot/js/site.js
+++ MovieReview/wwwroot/js/site.js
@@ -1,24 +1,71 @@
 ﻿
 
-$(document).ready(function () {
+document.addEventListener("DOMContentLoaded",function (e) {
 
 
+    let teste =  document.querySelector(".test")
+   
+    teste.addEventListener('click', () => {
 
 
-    $(".test").click(() => {
-        let data = $('#movie-form').serialize();
+        let form = document.getElementById("movie-form");
+    
+        let data = new FormData(form); //serialize 
+
+        let genres = document.querySelectorAll(".genres");
+
+        data.append('genreId', genre.value);
+
+
+        /*
+        for (genre of genres)
+        {
+            data.append('genreId', genre.value);
+        }*/
+       
+     
+        
         console.log(data);
 
+
+        for (const pair of data.entries())
+        {
+            console.log(`${pair[0]}, ${pair[1]}`);
+        }
+
+
+
+          /*
+        console.log(data.get('Title'));
+        console.log()
+
+
+      
+
+        let data = document.querySelector('#movie-form').serializeArray();
+
+
+
         let genres = [];
-        console.log($(".genres"));
 
-        $(".genres").each((index) => {
+        let allGenres = document.querySelectorAll(".genres");
 
-            console.log($(this).val());
-           // genres.push($(this).val());
+        console.log(allGenres); */
 
-        })
-       // console.log(genres);
+
+        /*
+
+        $(".genres").each((index,element) => {
+
+            genres.push($(element).val());
+
+        }) 
+
+        data.push(genres);
+
+        console.log(data);*/
+
+        
 
     })
 
